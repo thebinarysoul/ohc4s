ff8bdbf46070db5f212bae2b5d66c07889d8cf28
thebinarysoul <willymyfriend@gmail.com>
2022-06-27 00:31:01 +0300

    Used type lambda instead type alias

--- src/main/scala/com/thebinarysoul/ohc4s/cache/DefaultCache.scala
+++ src/main/scala/com/thebinarysoul/ohc4s/cache/DefaultCache.scala
@@ -1,13 +1,12 @@
 package com.thebinarysoul.ohc4s.cache
 
-import com.thebinarysoul.ohc4s.cache.DefaultCache.Id
 import com.thebinarysoul.ohc4s.codec.{Codec, Serializer}
 import org.caffinitas.ohc.{OHCache, OHCacheBuilder}
 import util.chaining.scalaUtilChainingOps
 
 import java.nio.ByteBuffer
 
-case class DefaultCache[K, V](capacity: Option[Long] = None)(using keyCodec: Codec[K], valueCodec: Codec[V]) extends Cache[Id, K, V] {
+class DefaultCache[K, V](capacity: Option[Long] = None)(using keyCodec: Codec[K], valueCodec: Codec[V]) extends Cache[[T] =>> T, K, V] {
   override protected val cache: OHCache[ByteBuffer, ByteBuffer] = OHCacheBuilder
     .newBuilder[ByteBuffer, ByteBuffer]
     .keySerializer(Serializer)
@@ -18,10 +17,6 @@ case class DefaultCache[K, V](capacity: Option[Long] = None)(using keyCodec: Cod
   private implicit inline def encode[T](value: T)(using codec: Codec[T]): ByteBuffer = codec.encode(value)
   private implicit inline def decode[T](buffer: ByteBuffer)(using codec: Codec[T]): Option[T] = Option(buffer).map(codec.decode)
 
-  override def put(key: K, value: V): Id[Boolean] = cache.put(key, value)
-  override def get(key: K): Id[Option[V]] = cache.get(key)
-}
-
-object DefaultCache {
-  type Id[T] = T
+  override def put(key: K, value: V): Boolean = cache.put(key, value)
+  override def get(key: K): Option[V] = cache.get(key)
 }
