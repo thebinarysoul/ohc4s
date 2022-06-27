83c29cca0bb1c5c76feb0595d83c799292afc687
thebinarysoul <willymyfriend@gmail.com>
2022-06-27 15:22:29 +0300

    Used the union of base size types instead constants

--- src/main/scala/com/thebinarysoul/ohc4s/codec/Codec.scala
+++ src/main/scala/com/thebinarysoul/ohc4s/codec/Codec.scala
@@ -1,7 +1,7 @@
 package com.thebinarysoul.ohc4s.codec
 
 import com.thebinarysoul.ohc4s.codec.Codec.*
-import com.thebinarysoul.ohc4s.codec.Sizes.*
+import com.thebinarysoul.ohc4s.codec.Size.*
 
 import java.nio.ByteBuffer as Buffer
 import scala.language.implicitConversions
@@ -25,38 +25,38 @@ object Codec {
 }
 
 given Codec[Byte] with
-  override def encode(value: Byte): Buffer = encodeWith(ONE)(_.put(value))
+  override def encode(value: Byte): Buffer = encodeWith(int[1])(_.put(value))
   override def decode(buffer: Buffer): Byte = buffer.get
 
 given Codec[Short] with
-  override def encode(value: Short): Buffer = encodeWith(TWO)(_.putShort(value))
+  override def encode(value: Short): Buffer = encodeWith(int[2])(_.putShort(value))
   override def decode(buffer: Buffer): Short = buffer.getShort
 
 given Codec[Int] with
-  override def encode(value: Int): Buffer = encodeWith(FOUR)(_.putInt(value))
+  override def encode(value: Int): Buffer = encodeWith(int[4])(_.putInt(value))
   override def decode(buffer: Buffer): Int = buffer.getInt
 
 given Codec[Long] with
-  override def encode(value: Long): Buffer = encodeWith(EIGHT)(_.putLong(value))
+  override def encode(value: Long): Buffer = encodeWith(int[8])(_.putLong(value))
   override def decode(buffer: Buffer): Long = buffer.getLong
 
 given Codec[Float] with
-  override def encode(value: Float): Buffer = encodeWith(FOUR)(_.putFloat(value))
+  override def encode(value: Float): Buffer = encodeWith(int[4])(_.putFloat(value))
   override def decode(buffer: Buffer): Float = buffer.getFloat
 
 given Codec[Double] with
-  override def encode(value: Double): Buffer = encodeWith(EIGHT)(_.putDouble(value))
+  override def encode(value: Double): Buffer = encodeWith(int[8])(_.putDouble(value))
   override def decode(buffer: Buffer): Double = buffer.getDouble
 
 given Codec[Boolean] with
-  override def encode(value: Boolean): Buffer = encodeWith(ONE)(_.put(if value then ONE else ZERO))
-  override def decode(buffer: Buffer): Boolean = buffer.get == ONE
+  override def encode(value: Boolean): Buffer = encodeWith(int[1])(_.put(if value then byte[1] else byte[0]))
+  override def decode(buffer: Buffer): Boolean = buffer.get == int[1]
 
 given Codec[String] with
   override def encode(value: String): Buffer =
     val bytes = value.getBytes
     merge(
-      encodeWith(FOUR)(_.putInt(bytes.length)),
+      encodeWith(int[4])(_.putInt(bytes.length)),
       encodeWith(bytes.length)(_.put(bytes))
     )
 
@@ -66,24 +66,24 @@ given Codec[String] with
     buffer.get(array)
     String(array)
 
-implicit inline def optCodec[T](using codec: Codec[T]): Codec[Option[T]] = new Codec[Option[T]] :
+implicit inline def optCodec[T](using codec: Codec[T]): Codec[Option[T]] = new Codec[Option[T]]:
   override def encode(value: Option[T]): Buffer = value match {
     case Some(value) =>
       merge(
-        encodeWith(ONE)(_.put(ONE)),
+        encodeWith(int[1])(_.put(byte[1])),
         codec.encode(value)
       )
-    case _ => encodeWith(ONE)(_.put(ZERO))
+    case _ => encodeWith(int[1])(_.put(byte[0]))
   }
 
   override def decode(buffer: Buffer): Option[T] =
-    if buffer.get == ONE
+    if buffer.get == int[1]
     then Some(codec.decode(buffer))
     else None
 
-implicit inline def seqCodec[T](using codec: Codec[T]): Codec[List[T]] = new Codec[List[T]] :
+implicit inline def seqCodec[T](using codec: Codec[T]): Codec[List[T]] = new Codec[List[T]]:
   override def encode(value: List[T]): Buffer =
-    val sizeBuffer = encodeWith(FOUR)(_.putInt(value.size))
+    val sizeBuffer = encodeWith(int[4])(_.putInt(value.size))
     value
       .map(codec.encode)
       .foldLeft(sizeBuffer)((a, b) => merge(a, b))
@@ -92,9 +92,9 @@ implicit inline def seqCodec[T](using codec: Codec[T]): Codec[List[T]] = new Cod
     val size = buffer.getInt
     List.fill(size)(codec.decode(buffer))
 
-implicit inline def mapCodec[K, V](using keyCodec: Codec[K], valueCodec: Codec[V]): Codec[Map[K, V]] = new Codec[Map[K, V]] :
+implicit inline def mapCodec[K, V](using keyCodec: Codec[K], valueCodec: Codec[V]): Codec[Map[K, V]] = new Codec[Map[K, V]]:
   override def encode(value: Map[K, V]): Buffer = {
-    val sizeBuffer = encodeWith(FOUR)(_.putInt(value.size))
+    val sizeBuffer = encodeWith(int[4])(_.putInt(value.size))
     value
       .map { case (key, value) => merge(keyCodec.encode(key), valueCodec.encode(value)) }
       .foldLeft(sizeBuffer)((a, b) => merge(a, b))
--- src/main/scala/com/thebinarysoul/ohc4s/codec/Sizes.scala
+++
@@ -1,9 +0,0 @@
-package com.thebinarysoul.ohc4s.codec
-
-object Sizes {
-  val ZERO: Byte = 0
-  val ONE: Byte = 1
-  val TWO: Byte = 2
-  val FOUR: Byte = 4
-  val EIGHT: Byte = 8
-}
